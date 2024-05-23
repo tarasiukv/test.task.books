35f9fa48614db78a9e052cfe84b5e80ee4b6ff41
tarasiukv <tarasiuk.viktor.o.m@gmail.com>
2024-05-23 14:17:36 +0300

    New:  - home component

---
+++ resources/js/src/component/HomeComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="blog-details spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-7 order-md-1 order-1">
+                    <div class="blog__details__text">
+                        <h3>Welcome to our Boooooooks!</h3>
+                        <p>Explore our collection of books and add new ones to our catalog.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
