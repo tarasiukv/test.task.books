475e1d683ba74fb2d892b2555937a75b639cd76e
tarasiukv <tarasiuk.viktor.o.m@gmail.com>
2024-05-23 14:23:22 +0300

    New:  - src/component: BookDetailComponent

---
+++ resources/js/src/component/BookDetailComponent.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {onMounted} from 'vue';
+import {useRoute} from 'vue-router';
+import useBooks from "@composable/book.js";
+
+const route = useRoute();
+const {book, getBook} = useBooks();
+
+const book_id = route.params.id;
+
+onMounted(async () => {
+    await getBook(book_id);
+});
+</script>
+
+<template>
+    <section class="product-details">
+        <div class="container">
+            <div class="product__details__text">
+                <h3>{{ book.title }}</h3>
+                <div class="product__details__price">{{ book.price }}</div>
+                <ul>
+                    <li><b>Publisher:</b> <span>{{book.publisher.name }}</span></li>
+                    <li><b>Author:</b> <span>{{ book.author.name }}</span></li>
+                    <li><b>Status:</b> <span>{{ book.status }}</span></li>
+                </ul>
+            </div>
+            <div class="container">
+                <div class="product__details__tab">
+                    <h6>Description of book</h6>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+
+</style>
